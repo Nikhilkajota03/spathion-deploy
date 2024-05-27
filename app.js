e0d981e682b770924633754a770fcf050ae4b43b
Nikhilkajota03 <117473576+Nikhilkajota03@users.noreply.github.com>
2024-05-27 22:27:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 
 
 mongoose
-.connect("mongodb+srv://nikhilkajota9413750125:MNrlh4mJbp7kDLvl@spathion-backend.wn5hg3x.mongodb.net/", {
+.connect("https://clever-klepon-0c9a51.netlify.app", {
   dbName: "spation-backend",
 })
 .then(() => {
