e6c70dbe60c6afdbe9beacb77e761b69eaa4a718
Nikhilkajota03 <117473576+Nikhilkajota03@users.noreply.github.com>
2024-05-27 21:59:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 
 
 mongoose
-.connect("mongodb://localhost:27017", {
+.connect("mongodb+srv://nikhilkajota9413750125:MNrlh4mJbp7kDLvl@spathion-backend.wn5hg3x.mongodb.net/", {
   dbName: "spation-backend",
 })
 .then(() => {
